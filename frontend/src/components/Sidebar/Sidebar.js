7e8f2f9c492d0b52a110363a93d230458ad0c425
Nazhim <nazhimkalam@gmail.com>
2020-09-06 19:31:40 +0530

    added the sidebar header icons

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import './Sidebar.css'
+import React from 'react';
+import './Sidebar.css';
+import { IconButton } from '@material-ui/core';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function Sidebar() {
-    return (
-        <div className="sideBar">
-            <h1>Sidebar</h1>
-        </div>
-    )
+	return (
+		<div className="sideBar">
+			<div className="sideBar__header">
+				<Avatar src={user?.photoURL} />
+				<div className="sideBar__headerRight">
+					<IconButton>
+						<DonutLargeIcon />
+					</IconButton>
+					<IconButton>
+						<ChatIcon />
+					</IconButton>
+					<IconButton>
+						<MoreVertIcon />
+					</IconButton>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Sidebar
+export default Sidebar;
