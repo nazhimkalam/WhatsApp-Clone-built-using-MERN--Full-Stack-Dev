9ca7a9919ca3496fb358f67cb3fe24ec4942567a
Nazhim <nazhimkalam@gmail.com>
2020-09-06 18:28:07 +0530

    created the sidebar and chat component template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
+import Chat from './components/Chat/Chat';
 
 function App() {
-  return (
-    <div className="app">
-     
-    </div>
-  );
+	return (
+		<div className="app">
+			<Sidebar />
+			<Chat />
+		</div>
+	);
 }
 
 export default App;
+
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
 	apiKey: 'AIzaSyD_MdXKw8dVdc3Mh8oVU3JyGVvBJiByMQY',
 	authDomain: 'whatsapp-mern-5c5a3.firebaseapp.com',
@@ -8,3 +10,4 @@ const firebaseConfig = {
 	appId: '1:10779015067:web:d215a5e3a34ff5310ed33e',
 	measurementId: 'G-0T16306Z4K',
 };
+
