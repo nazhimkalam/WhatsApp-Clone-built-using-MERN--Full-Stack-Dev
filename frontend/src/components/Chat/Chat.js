c0279cd5424cd49855b02651b560a33ed52204eb
Nazhim <nazhimkalam@gmail.com>
2020-09-06 20:12:02 +0530

    created the chat footer

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Chat.css';
 import { Avatar, IconButton } from '@material-ui/core';
 import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 import AttachFileOutlinedIcon from '@material-ui/icons/AttachFileOutlined';
 import MoreVertOutlinedIcon from '@material-ui/icons/MoreVertOutlined';
-
+import MicIcon from '@material-ui/icons/Mic';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 
 function Chat() {
+    const [input, setInput] = useState('');
+    
+	const sendMessage = (e) => {
+		e.preventDefault();
+	};
+
 	return (
 		<div className="chat">
 			<div className="chat__header">
@@ -16,7 +23,7 @@ function Chat() {
 					<p>last seen . . . </p>
 				</div>
 
-                <div className="chat__headerRight">
+				<div className="chat__headerRight">
 					<IconButton>
 						<SearchOutlinedIcon />
 					</IconButton>
@@ -31,8 +38,35 @@ function Chat() {
 				</div>
 			</div>
 
+			<div className="chat__body">
+				<p className="chat__message">
+					<span className="chat__name" style={{ color: `hsl(${Math.floor(Math.random() * 361)}, 85%, 45%)` }}>
+						Nazhim Kalam
+					</span>
+					This is a message
+					<span className="chat__timestamp">{new Date().toUTCString()}</span>
+				</p>
+			</div>
 
-            
+			<div className="chat__footer">
+				<IconButton>
+					<InsertEmoticonIcon />
+				</IconButton>
+				<form>
+					<input
+						placeholder="Type a message"
+						type="text"
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+					/>
+					<button type="submit" onClick={sendMessage}>
+						send
+					</button>
+				</form>
+				<IconButton>
+					<MicIcon />
+				</IconButton>
+			</div>
 		</div>
 	);
 }
