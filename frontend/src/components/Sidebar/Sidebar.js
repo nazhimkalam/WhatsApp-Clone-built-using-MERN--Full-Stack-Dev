69744cb16949464f1a33f2defdb702afffd27f41
Nazhim <nazhimkalam@gmail.com>
2020-09-06 19:35:41 +0530

    created the sideBarChat component

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Sidebar.css';
-import { IconButton } from '@material-ui/core';
+import { IconButton, Avatar } from '@material-ui/core';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 
 function Sidebar() {
 	return (
 		<div className="sideBar">
 			<div className="sideBar__header">
-				<Avatar src={user?.photoURL} />
+				<Avatar />
 				<div className="sideBar__headerRight">
 					<IconButton>
 						<DonutLargeIcon />
@@ -21,6 +22,20 @@ function Sidebar() {
 						<MoreVertIcon />
 					</IconButton>
 				</div>
+			</div>
+
+			<div className="sideBar__search">
+				<div className="sideBar__searchContainer">
+					<SearchOutlinedIcon />
+					<input type="text" placeholder="Search or start new group" />
+				</div>
+			</div>
+
+			<div className="sideBar__chats">
+				<SideBarChat addNewChat />
+				<SideBarChat />
+				<SideBarChat />
+				<SideBarChat />
 			</div>
 		</div>
 	);
