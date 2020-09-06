47d76e3eb8606dc7adce5edf3dd38a173552bf67
Nazhim <nazhimkalam@gmail.com>
2020-09-07 00:50:52 +0530

    using pusher we get the data to fronend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
 import Chat from './components/Chat/Chat';
+import Pusher from 'pusher-js'
 
 function App() {
+	useEffect(() => {
+		// using the backend pusher we are getting the data from mongodb in realtime
+		const pusher = new Pusher('4c3e84c47532a02a357d', {
+			cluster: 'eu',
+		});
+
+		const channel = pusher.subscribe('messages');
+		channel.bind('inserted', (data) => {
+			alert(JSON.stringify(data));
+		});
+
+	}, []);
 	return (
 		<div className="app">
 			<div className="app__body">
