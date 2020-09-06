15a4704a03b9c6e73497cc3cff7db821729aaada
Nazhim <nazhimkalam@gmail.com>
2020-09-06 18:17:00 +0530

    completed the clean up process and created the necessary folders to start off

---
+++ frontend/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyD_MdXKw8dVdc3Mh8oVU3JyGVvBJiByMQY',
+	authDomain: 'whatsapp-mern-5c5a3.firebaseapp.com',
+	databaseURL: 'https://whatsapp-mern-5c5a3.firebaseio.com',
+	projectId: 'whatsapp-mern-5c5a3',
+	storageBucket: 'whatsapp-mern-5c5a3.appspot.com',
+	messagingSenderId: '10779015067',
+	appId: '1:10779015067:web:d215a5e3a34ff5310ed33e',
+	measurementId: 'G-0T16306Z4K',
+};
