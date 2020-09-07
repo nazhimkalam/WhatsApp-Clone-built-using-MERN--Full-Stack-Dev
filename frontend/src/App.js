c910b8103739f4aecd80afd8847d156cd1bce2bc
Nazhim <nazhimkalam@gmail.com>
2020-09-07 10:52:40 +0530

    fetching data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
 import Chat from './components/Chat/Chat';
 import Pusher from 'pusher-js';
+import axios from './axios';
 
 function App() {
+	const [messages, setMessages] = useState([]);
 
 	useEffect(() => {
-	
-	}, [])
+		axios.get('/messages/sync').then((response) => {
+			setMessages(response.data);
+		});
+	}, []);
 
 	useEffect(() => {
 		// using the backend pusher we are getting the data from mongodb in realtime
@@ -21,6 +25,8 @@ function App() {
 			alert(JSON.stringify(data));
 		});
 	}, []);
+
+	console.log(messages);
 
 	
 	return (
