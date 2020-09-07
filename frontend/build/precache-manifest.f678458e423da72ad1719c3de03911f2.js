0f995a40ffcb2bd752697367044c5163d4cdf63c
Nazhim <nazhimkalam@gmail.com>
2020-09-07 13:59:32 +0530

    firebase deployed

---
+++ frontend/build/precache-manifest.f678458e423da72ad1719c3de03911f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e40a95ea3a711bb6e152e5e103520d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aea107b4ca625b567d6",
+    "url": "/static/css/main.1c2fa816.chunk.css"
+  },
+  {
+    "revision": "e31ab811b1243ad6ec49",
+    "url": "/static/js/2.a07388aa.chunk.js"
+  },
+  {
+    "revision": "95e5393dbc94b774bee63119a26602ef",
+    "url": "/static/js/2.a07388aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aea107b4ca625b567d6",
+    "url": "/static/js/main.76102316.chunk.js"
+  },
+  {
+    "revision": "05ca6a9c9285167c634e",
+    "url": "/static/js/runtime-main.0502b7a5.js"
+  }
+]);
